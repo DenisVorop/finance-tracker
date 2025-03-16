d9b2953bae415ef0306b1d82580787eb1cca077a
DenisVorop <saskeazaske@icloud.com>
2025-03-17 00:33:46 +0300

    init

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from "@/lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      return await GET(req, res);
+    case "POST":
+      return await POST(req, res);
+    case "PUT":
+      return await PUT(req, res);
+    case "DELETE":
+      return await DELETE(req, res);
+    default:
+      throw new Error("Unknown method");
+  }
+}
+
+async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const users = await prisma.user.findMany();
+
+  return res.status(200).json(users);
+}
+
+async function POST(req: NextApiRequest, res: NextApiResponse) {}
+
+async function PUT(req: NextApiRequest, res: NextApiResponse) {}
+
+async function DELETE(req: NextApiRequest, res: NextApiResponse) {}
---
+++ src/pages/index.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const [position, setPosition] = useState("bottom");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("/api/hello");
+        const data = await res.json();
+
+        console.log(data);
+      } catch (err) {
+        // .keep
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
