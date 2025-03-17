d053c1939964251b34291a285a22947238b74f33
DenisVorop <saskeazaske@icloud.com>
2025-03-18 00:04:21 +0300

    add signin page

---
+++ src/pages/signin/page/signin.page.tsx
@@ -0,0 +1,9 @@
+import { SignInForm } from "@/components/features";
+
+export function SignInPage() {
+  return (
+    <div className="max-w-96 w-full ml-auto mr-auto flex flex-1 items-center">
+      <SignInForm />
+    </div>
+  );
+}
