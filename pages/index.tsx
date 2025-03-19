f07b62acb68be4030ebf8119895daa59b81cf50a
DenisVorop <saskeazaske@icloud.com>
2025-03-19 22:55:03 +0300

    add global middleware & add features dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,1 @@
-import { StateBuilder } from "@/app/lib/state-builder";
-import { exampleApi } from "@/shared/api/example";
-import { ExamplePage } from "@/pages/example";
-import { protectedRoute } from "common/lib/protected-route";
-
-export default ExamplePage;
-
-export const getServerSideProps = protectedRoute(
-  async (ctx: App.PageContext) => {
-    const example = await exampleApi.getExample();
-
-    ctx.req.__EXAMPLE__ = example;
-
-    const initialState = StateBuilder.Init(ctx).setBaseData().build();
-
-    return { props: { initialState } };
-  }
-);
+export default () => {};
