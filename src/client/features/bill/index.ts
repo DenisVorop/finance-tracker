b447eeee4b78cee24e63a0256d25e8d795aadc87
DenisVorop <saskeazaske@icloud.com>
2025-03-28 20:32:27 +0300

    fix: imports

--- src/client/features/bill/index.ts
+++ src/client/features/bill/index.ts
@@ -1,3 +1,5 @@
 export { BillHeader } from "./ui/bill-header";
 export { BillInfo } from "./ui/bill-info";
 export { BillActions } from "./ui/bill-actions";
+
+export { withBillProviders } from "./providers/with-bill-providers";
