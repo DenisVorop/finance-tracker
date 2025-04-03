0d0a7a87ab8736097f80d06d0961b87601691687
DenisVorop <saskeazaske@icloud.com>
2025-04-04 01:59:39 +0300

    refactor: consistent forms

--- src/client/features/signin/ui/signin-form/index.tsx
+++ src/client/features/signin/ui/signin-form/index.tsx
@@ -5,19 +5,11 @@ import { useCallback } from "react";
 
 import { Button } from "@/shared/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/shared/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/shared/ui/form";
-import { Input } from "@/shared/ui/input";
 import { useSignin } from "@/entities/auth";
 import { navigate } from "@/shared/lib/navigate";
 import type { SignInFormData } from "common/schemas/signin.schema";
 import { signInSchema } from "common/schemas/signin.schema";
+import { FormControl } from "@/shared/ui/form-control";
 
 export function SignInForm() {
   const { mutateAsync } = useSignin({
@@ -47,55 +39,45 @@ export function SignInForm() {
         <CardTitle>Вход в аккаунт</CardTitle>
       </CardHeader>
       <CardContent>
-        <Form {...form}>
-          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="ivanov@yandex.ru" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Пароль</FormLabel>
-                  <FormControl>
-                    <Input placeholder="******" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="text-right text-sm">
-              <Link
-                href="/forgot-password"
-                className="text-blue-500 hover:underline"
-              >
-                Забыли пароль?
-              </Link>
-            </div>
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormControl
+            control={form.control}
+            controlLabel="Email"
+            controlType="text"
+            type="email"
+            name="email"
+            placeholder="ivanov@yandex.ru"
+          />
 
-            <Button type="submit" className="w-full">
-              Войти
-            </Button>
+          <FormControl
+            control={form.control}
+            controlLabel="Пароль"
+            controlType="text"
+            type="password"
+            name="password"
+            placeholder="******"
+          />
 
-            <div className="text-center text-sm">
-              У вас ещё нет аккаунта?{" "}
-              <Link href="/signup" className="text-blue-500 hover:underline">
-                Зарегистрироваться
-              </Link>
-            </div>
-          </form>
-        </Form>
+          <div className="text-right text-sm">
+            <Link
+              href="/forgot-password"
+              className="text-blue-500 hover:underline"
+            >
+              Забыли пароль?
+            </Link>
+          </div>
+
+          <Button type="submit" className="w-full">
+            Войти
+          </Button>
+
+          <div className="text-center text-sm">
+            У вас ещё нет аккаунта?{" "}
+            <Link href="/signup" className="text-blue-500 hover:underline">
+              Зарегистрироваться
+            </Link>
+          </div>
+        </form>
       </CardContent>
     </Card>
   );
