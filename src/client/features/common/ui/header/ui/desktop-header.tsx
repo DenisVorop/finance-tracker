51856990ac376f5fb87b4b98774a9e803e46f535
DenisVorop <saskeazaske@icloud.com>
2025-04-22 20:27:54 +0300

    feat: add logo

--- src/client/features/common/ui/header/ui/desktop-header.tsx
+++ src/client/features/common/ui/header/ui/desktop-header.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/shared/ui/button";
 import { cn } from "@/shared/lib/utils";
 
 import { config } from "../lib/config";
+import logo from "../assets/finance-tracker-logo.png";
 
 import { Avatar } from "./avatar";
 
@@ -14,7 +15,10 @@ export function DesktopHeader() {
   return (
     <header className="sticky p-4 shadow-sm top-0 backdrop-blur-sm border-b">
       <div className="container flex items-center justify-between">
-        <div className="text-xl font-bold">Логотип</div>
+        <div className="flex items-center gap-2">
+          <img src={logo.src} width={36} height={36} className="rounded-xl" />
+          <div className="text-xl font-bold">Finance +</div>
+        </div>
 
         <nav className="flex items-center gap-4">
           {config.map(({ Icon, label, link }) => (
